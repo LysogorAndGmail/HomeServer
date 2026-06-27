f4cef0c09d44c27962886acc798fc099c5708e0f
LysogorAndGmail <52484321+LysogorAndGmail@users.noreply.github.com>
2026-06-27 12:21:04 +0200

    Admin add buttons 23

--- my-vue-test/src/components/Admin.vue
+++ my-vue-test/src/components/Admin.vue
@@ -237,9 +237,14 @@ onMounted(() => {
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
-        <div class="form-check form-switch">
-          <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-          <label class="form-check-label" for="flexSwitchCheckDefault">Ozuchka</label>
+        <div class="form-check form-switch ms-3 me-3">
+          <input
+              v-model="isOzvuchkaEnabled"
+              class="form-check-input"
+              type="checkbox"
+              id="flexSwitchCheckDefault"
+          >
+          <label class="form-check-label" for="flexSwitchCheckDefault">Ozvuchka</label>
         </div>
         <div class="btn-toolbar mb-2 mb-md-0">
           <button type="button" class="btn btn-sm btn-outline-secondary" @click="loadApiLogs">
