79c3fae020d5f0a685aba0da40da88a49b70e832
LysogorAndGmail <52484321+LysogorAndGmail@users.noreply.github.com>
2026-07-05 18:40:27 +0200

    pogoda 4

--- my-spring-app/src/main/java/com/example/demo/WeatherService.java
+++ my-spring-app/src/main/java/com/example/demo/WeatherService.java
@@ -9,9 +9,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class WeatherService {
 
     // Вставь сюда свой API-ключ от OpenWeatherMap
-    private static final String API_KEY = "b4d7724ae35984c01952e44da7dd3845";
+    private static final String API_KEY = "e08d0a28190ccabf76290259d3e6b05d";
     // Указываем твой город (можно написать "Tubingen,DE" или "Dettenhausen,DE")
-    private static final String CITY = "Tubingen,DE";
+    private static final String CITY = "Dettenhausen,DE";
     private static final String URL = "https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&appid=" + API_KEY + "&units=metric&lang=ru";
 
     private final RestTemplate restTemplate;
